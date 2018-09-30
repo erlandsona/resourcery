9e27b4e17a42b12ad799038468d5cc8b21083754
Austin Erlandson <austin@erlandson.com>
2018-09-30 17:03:36 -0500

    Adds Data.Yaml.Config and WIP: Data.Pool implementation.

--- api/library/API.hs
+++ api/library/API.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE StandaloneDeriving    #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeApplications      #-}
@@ -12,17 +13,21 @@
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
-module Lib where
+module API where
 
 import Data.Aeson
 import Data.Text (Text)
 import Database.Beam
+import Database.PostgreSQL.Simple
+import GHC.Word (Word16)
 -- import Database.Beam.Postgres
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
+import Data.Pool
+import Data.Yaml.Config (loadYamlSettings, useEnv)
 
--- Database Configs
+-- Database Schema
 data UserT f
     = User
     { userId        :: Columnar f Int
@@ -74,6 +79,37 @@ api = Proxy
 server :: Server API
 server = return []
 
+data DBConfig = DBConfig
+    { connectPort     :: Word16
+    , connectHost     :: String
+    , connectUser     :: String
+    , connectPassword :: String
+    , connectDatabase :: String
+    }
+    deriving (Generic, FromJSON)
+
+data ServerConfig = ServerConfig
+    { port :: Int
+    } deriving (Generic, FromJSON)
+
+data Config = Config
+    { serverConfig :: ServerConfig
+    , dbConfig :: DBConfig
+    } deriving (Generic, FromJSON)
+
 -- Main
-startApp :: IO ()
-startApp = run 8080 app
+main :: IO ()
+main = do
+    Config { serverConfig = ServerConfig{..}, ..} <- loadYamlSettings ["config/settings.yaml"] [] useEnv
+    pool <- makePool dbConfig
+
+    run port app
+
+makePool :: DBConfig -> IO (Pool Connection)
+makePool DBConfig{..} =
+    let
+        connectionTimeout = 60 -- Seconds
+        connectionsPerStripe = 10
+        stripes = 2
+    in
+        createPool (connect ConnectInfo{..}) close stripes connectionTimeout connectionsPerStripe
